41a66fc82de18cb7513a023cbb017e783124f7de
Spencer Anderson <se.anderson88@gmail.com>
2020-03-12 12:29:57 -0500

    Watched videos 2-4. CRU implemented

--- app.py
+++ app.py
@@ -1,36 +1,228 @@
 #!/usr/local/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from hashlib import md5
+from datetime import datetime
+from functools import wraps
+
 
 app = Flask(__name__)
 
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-Articles = Articles()
+# Init MySQL
+mysql = MySQL(app)
 
+
+# Articles = Articles()
+
+# Index
 @app.route('/')
 def index():
     return render_template('home.html')
 
-
+# About
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+# Articles
 @app.route('/articles')
 def articles():
-    return render_template('articles.html', articles=Articles)
+    # Create cursor
+    cur = mysql.connection.cursor()
 
+    # Get articles
+    result = cur.execute("SELECT * FROM articles")
+
+    articles = cur.fetchall()
+
+    if result > 0:
+        return render_template('articles.html', articles=articles)
+    else:
+        msg = 'No Articles Found'
+        return render_template('articles.html', msg=msg)
+    # Close connection
+    cur.close()
+
+
+# Single article
 @app.route('/article/<string:id>/')
 def article(id):
-    return render_template('article.html', id=id)
 
-@app.route('/register')
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get article
+    result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+
+    article = cur.fetchone()
+    return render_template('article.html', article=article)
+
+
+# User Registration form class
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=80)])
+    password = PasswordField('Password', [
+        validators.data_required(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+# User registration
+@app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('register.html')
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
 
-@app.route('/login')
+        # Create cursor
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You are now registered! You can login', 'success')
+
+        return redirect(url_for('login'))  
+
+        return render_template('register.html', form=form)
+    return render_template('register.html', form=form)
+
+# User login
+@app.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        # Get form fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+        
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Either your username or password is incorrect.'
+                return render_template('login.html', error=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+
     return render_template('login.html')
+
+# Check if user is logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+# Logout
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash("You're now logged out", 'success')
+    return redirect(url_for('login'))
+
+# Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get articles
+    # Show articles only from the user logged in 
+    result = cur.execute("SELECT * FROM articles WHERE author = %s", [session['username']])
+
+    articles = cur.fetchall()
+
+    if result > 0:
+        return render_template('dashboard.html', articles=articles)
+    else:
+        msg = 'No Articles Found'
+        return render_template('dashboard.html', msg=msg)
+    # Close connection
+    cur.close()
+
+
+
+# Article Form Class
+class ArticleForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=200)])
+    body = TextAreaField('Body', [validators.Length(min=30)])
+
+
+# Add Article
+@app.route('/add_article', methods=['GET', 'POST'])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+        
+        # Execute query
+        cur.execute('INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)', (title, body, session['username']))
+
+        # Save changes to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('Article created', 'success')
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_article.html', form=form)
+
+
+
 if __name__ == '__main__':
+    app.secret_key = md5((datetime(1988,4,28,0,0).strftime('%s').encode('utf-8'))).hexdigest()
     app.run(debug=True)
--- data.py
+++ data.py
@@ -3,24 +3,33 @@ def Articles():
         {
             'id': 1,
             'title': 'Article One',
-            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, consequuntur nostrum quod omnis, rem nesciunt neque explicabo sit officiis repellat veritatis perspiciatis qui debitis. Iure, exercitationem explicabo! Odit, dignissimos fugiat.',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, consequuntur \
+                nostrum quod omnis, rem nesciunt neque explicabo sit officiis \
+                repellat veritatis perspiciatis qui debitis. \
+                Iure, exercitationem explicabo! Odit, dignissimos fugiat.',
             'author': 'Spencer',
             'create date': '02-25-2020'
         },
         {
             'id': 2,
             'title': 'Article Two',
-            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, consequuntur nostrum quod omnis, rem nesciunt neque explicabo sit officiis repellat veritatis perspiciatis qui debitis. Iure, exercitationem explicabo! Odit, dignissimos fugiat.',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, consequuntur \
+                nostrum quod omnis, rem nesciunt neque explicabo sit officiis \
+                repellat veritatis perspiciatis qui debitis. \
+                Iure, exercitationem explicabo! Odit, dignissimos fugiat.',
             'author': 'Johnny',
             'create date': '02-25-2020'
         },
         {
             'id': 3,
             'title': 'Article Three',
-            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, consequuntur nostrum quod omnis, rem nesciunt neque explicabo sit officiis repellat veritatis perspiciatis qui debitis. Iure, exercitationem explicabo! Odit, dignissimos fugiat.',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, consequuntur \
+                nostrum quod omnis, rem nesciunt neque explicabo sit officiis \
+                repellat veritatis perspiciatis qui debitis. \
+                Iure, exercitationem explicabo! Odit, dignissimos fugiat.',
             'author': 'Kyle',
             'create date': '02-25-2020'
         }
     ]
 
-    return articles 
+    return articles
